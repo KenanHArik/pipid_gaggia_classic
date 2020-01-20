8148b579b7971d162ae06f905f8132d852647384
KenanHArik <arik.kenan@gmail.com>
2020-01-20 10:07:02 -0800

    websockets tutorial final

--- Tutorials/Websockets Part 2/main.py
+++ Tutorials/Websockets Part 2/main.py
@@ -11,11 +11,9 @@ import time
 
 app = FastAPI()
 
-
 '''
 Homepage 
 '''
-
 @app.get("/")
 async def get():
     return FileResponse('static/index.html')
@@ -27,22 +25,17 @@ Websocket
 @app.websocket_route("/ws")
 class WSocket(WebSocketEndpoint):
     sleep = 1
-    encoding = 'text'
+    encoding = None
     
     async def on_connect(self, websocket):
         await websocket.accept()
-        # create asyncronous tasks:
+        # create async tasks here:
         self.task2 = asyncio.create_task(self.give_random(websocket))
-        # self.task1 = asyncio.create_task(self.print_state(websocket))
-        # await self.main(websocket)  # awaiting blocking code
     
     async def on_disconnect(self, websocket, close_code):
         self.task2.cancel()
         websocket.application_state = WebSocketState.DISCONNECTED
         assert websocket.client_state == WebSocketState.DISCONNECTED
-        # self.task1.cancel()
-        # print(websocket.client_state)
-        # print(websocket.application_state)
         print('Websocket Disconnected')
 
     async def on_receive(self, websocket, data):
@@ -57,9 +50,3 @@ class WSocket(WebSocketEndpoint):
         except Exception as e:
             pass
             # print(f'Exception encountered {e} while sending number')
-
-    # async def print_state(self, websocket):
-    #     while True:
-    #         print(f'print client state is {websocket.client_state}')
-    #         print(f'print application state is {websocket.application_state}')
-    #         await asyncio.sleep(1)
